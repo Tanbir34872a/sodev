1458f124d44ee29331d74844542358afd3c6870e
Tanbir Yousuf <yousuf.tanbir@gmail.com>
2025-04-10 18:24:21 +0600

    Create user module

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './entities/user.entity';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>,
+  ) {}
+  private readonly logger = new Logger(UserService.name);
+
+  async create(createUserDto: CreateUserDto) {
+    this.logger.log('Creating a new user');
+    this.logger.debug(`User data: ${JSON.stringify(createUserDto)}`);
+    const existingUser = await this.userModel
+      .findOne({
+        $or: [
+          { username: createUserDto.username },
+          { email: createUserDto.email },
+        ],
+      })
+      .exec();
+    if (existingUser) {
+      this.logger.warn('User already exists');
+      return {
+        message: 'User already exists',
+        status: 409,
+      };
+    }
+    this.logger.log('Hashing password');
+    const passwordHash = bcrypt.hashSync(createUserDto.password, 10);
+    const newUser = new this.userModel({
+      ...createUserDto,
+      password: passwordHash,
+    });
+    await newUser.save();
+    this.logger.log('User created successfully');
+    this.logger.debug(`New user data: ${JSON.stringify(newUser)}`);
+    return {
+      message: 'User created successfully',
+      status: 201,
+      user: newUser,
+    };
   }
 
   findAll() {
