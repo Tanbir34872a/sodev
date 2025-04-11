efa91db8149c97e092fd5c1992afb5e66c1831a0
Tanbir Yousuf <yousuf.tanbir@gmail.com>
2025-04-11 12:54:58 +0600

    Added login and authorization functionality

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,4 +1,7 @@
 import * as dotenv from 'dotenv';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('AppConfig');
 
 // Load environment variables from .env file
 dotenv.config();
@@ -6,19 +9,22 @@ dotenv.config();
 interface IAppConfig {
   PORT: number;
   MONGO_URI: string;
+  JWT_SECRET: string;
 }
 
 const getAppConfig = (): IAppConfig => {
   const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
   const mongoUri = process.env.MONGO_URI || '';
+  const jwtSecret = process.env.JWT_SECRET || 'meow';
   return {
     PORT: port,
     MONGO_URI: mongoUri,
+    JWT_SECRET: jwtSecret,
   };
 };
 
 const AppConfig = getAppConfig();
 
-console.log('AppConfig:', AppConfig);
+logger.log('AppConfig:', AppConfig);
 
 export default AppConfig;
